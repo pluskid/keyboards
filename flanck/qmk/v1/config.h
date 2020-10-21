c57f766d1ec4e1d7abb70cb0e995a54acdb41492
Chiyuan Zhang <pluskid@gmail.com>
2020-10-20 20:22:50 -0700

    flanck firmware and default keymap

---
+++ flanck/qmk/v1/config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x4E4B // NK
+#define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    pluskid
+#define PRODUCT         flanck
+#define DESCRIPTION     4x12 fat-planck
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
+#define MATRIX_COL_PINS { B1, B3, B2, B6, D3, D2, D4, C6, D7, E6, B4, B5 }
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
